0e6af761c6b3ab1dc2240de495bbd122094413ab
unknown <kainesv86@gmail.com>
2021-12-30 16:16:56 +0700

    Add - Form chat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,59 @@
 import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+        interface ChatLogProps {
+                user: number;
+                message: string;
+        }
+        const chatLog: Array<ChatLogProps> = [
+                { user: 0, message: "Hello" },
+                { user: 1, message: "Hi! My name is Kainé, K A I N É, I'm 20, FPT University student... and well, I'm dying" },
+        ];
+
+        const { register, handleSubmit } = useForm({ shouldUseNativeValidation: true });
+        const onSubmit = async (data: ChatLogProps) => {
+                console.log(data);
+        };
+
         return (
                 <div className="App">
                         <div className="bg-gray-700 h-screen p-40">
-                                <div className="bg-white h-full p-4 rounded items-start w-96">
-                                        <ul className="flex flex-col">
-                                                <li className="bg-blue-500 rounded-full py-2 px-4 font-medium w-fit text-right mb-1">Hello!</li>
-                                                <li className="bg-gray-300 rounded-full py-2 px-4 font-medium w-fit text-left mb-1">Hi!</li>
-                                        </ul>
+                                <div className="bg-white h-full rounded items-start w-96 relative">
+                                        <div className="p-4">
+                                                <ul className="flex flex-col">
+                                                        {chatLog.map((item, index) => (
+                                                                <li
+                                                                        key={index}
+                                                                        className={`flex ${
+                                                                                item.user !== 0 ? "justify-end ml-16" : "justify-start mr-16"
+                                                                        }`}
+                                                                >
+                                                                        <p
+                                                                                className={`${
+                                                                                        item.user !== 0 ? "bg-blue-500" : "bg-gray-300"
+                                                                                } rounded-3xl py-2 px-4 font-normal w-fit mb-1 text-left`}
+                                                                        >
+                                                                                {item.message}
+                                                                        </p>
+                                                                </li>
+                                                        ))}
+                                                </ul>
+                                        </div>
+                                        <form
+                                                className="bg-gray-800 bottom-0 right-0 absolute w-full h-16 px-3 items-center flex"
+                                                onSubmit={handleSubmit(onSubmit)}
+                                        >
+                                                <input
+                                                        type="text"
+                                                        className="bg-gray-100 rounded-3xl p-2 flex-1 outline-none px-4 w-auto mr-1"
+                                                        {...register("message")}
+                                                />
+                                                <input type="text" className="hidden" {...register("user")} value={0} />
+                                                <button type="submit" className="bg-gray-100 rounded-full p-2 h-10 w-10">
+                                                        +
+                                                </button>
+                                        </form>
                                 </div>
                         </div>
                 </div>
